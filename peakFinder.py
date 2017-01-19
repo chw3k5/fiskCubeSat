22f0466cc666e22db38a7501719eed8730469b3a
Caleb Wheeler <chw3k5@gmail.com>
2017-01-19 12:13:05 -0600

    added the file peakFinder

---
+++ peakFinder.py
@@ -0,0 +1,71 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Jeremiah
+#
+# Created:     23/06/2016
+# Copyright:   (c) Jeremiah 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#import numpy as np
+import matplotlib.pyplot as mpl
+import sys
+from numpy import NaN, Inf, arange, isscalar, asarray, array
+
+def peakdet(v, delta, x = None):
+
+    maxtab = []
+    mintab = []
+
+    if x is None:
+        x = arange(len(v))
+
+    v = asarray(v)
+
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+
+    if not isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+
+    if delta <= 0:
+        sys.exit('Input argument delta must be positive')
+
+    mn, mx = Inf, -Inf
+    mnpos, mxpos = NaN, NaN
+
+    lookformax = True
+
+    for i in range(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+
+        if lookformax:
+            if this < mx-delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn + delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+
+    return array(maxtab), array(mintab)
+
+if __name__=="__main__":
+    from matplotlib.pyplot import plot, scatter, show
+    series = [0,0,0,2,0,0,0,0,0,0,0,2,0,0,0,0,0]
+    maxtab, mintab = peakdet(series,.3)
+    plot(series)
+    scatter(array(maxtab)[:,0], array(maxtab)[:,1], color='blue')
+    scatter(array(mintab)[:,0], array(mintab)[:,1], color='red')
+    show()
