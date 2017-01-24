f5cabeab32960b4525e557cdf58273a586fff332
Caleb Wheeler <chw3k5@gmail.com>
2017-01-24 11:43:09 -0600

    I am trying to add the c and cpp code to be wrapped in python.

---
+++ cppDriverCode/spammodule.c
@@ -0,0 +1,36 @@
+//
+//  spammodule.c
+//  
+//
+//  Created by Caleb Wheeler on 1/23/17.
+//
+//
+
+#include "spammodule.h"
+
+
+static PyObject *
+spam_system(PyObject *self, PyObject *args)
+{
+    const char *command;
+    int sts;
+    
+    if (!PyArg_ParseTuple(args, "s", &command))
+        return NULL;
+    sts = system(command);
+    return Py_BuildValue("i", sts);
+}
+
+static PyMethodDef SpamMethods[] = {
+    ...
+    {"system",  spam_system, METH_VARARGS,
+        "Execute a shell command."},
+    ...
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+PyMODINIT_FUNC
+initspam(void)
+{
+    (void) Py_InitModule("spam", SpamMethods);
+}
---
+++ cppDriverCode/spammodule.h
@@ -0,0 +1,15 @@
+//
+//  spammodule.h
+//  
+//
+//  Created by Caleb Wheeler on 1/23/17.
+//
+//
+
+#ifndef spammodule_h
+#define spammodule_h
+
+#include <Python/Python.h>
+#include <stdio.h>
+
+#endif /* spammodule_h */
