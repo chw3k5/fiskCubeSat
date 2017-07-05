aa97468cdf10e730d791cf064cff6433232bc1f5
Caleb Wheeler <chw3k5@gmail.com>
2017-07-05 09:15:34 -0500

    Adding an introduction to python script.

---
+++ intoToPython.py
@@ -0,0 +1,153 @@
+"""
+Comments
+"""
+
+
+"""
+The print state statement
+"""
+
+# simple example
+
+# compound example
+
+# the \n (newline) character
+
+
+
+"""
+Numbers - basic single value numbers Integers and floats.
+"""
+
+### integers (no decimal point)
+
+# Tricky division
+
+# The modulus operator
+
+
+
+
+### float (floating point number, has a decimal point)
+
+# intuitive division.
+
+# conversion to an integer
+
+# conversion from an integer
+
+# when used with an integer in math
+
+
+
+
+"""
+Strings
+ Strings can be groups of numbers or letters that are enclosed in quotes.
+ Single quotes '' or double "" quotes will both work. However, if you use an apostrophe '
+ Then you must enclose the apostrophe in double quotes to be be used in the string.
+    For Example:
+    stringVar = "This is Caleb's string, ain't it grand?"
+"""
+
+# single quotes
+
+# double quotes
+
+# conversion to a number
+
+# concatenation
+
+# split method
+
+# replace method
+
+"""
+Tuples - pronounced 2-ple
+ These are a grouping of data in an immutable structure. Immutable means the length of the tuple
+ (I think of it as a container is not allowed to changed)
+ Tuples are enclosed in round brackets () and the values are separated by commas, the values in a tuple are
+ accessed using integers starting with 0. Access that last value in the tuple starting with -1.
+
+"""
+
+# coordinates example
+
+# unpacking and packing
+
+# calling a single value.
+
+# tuple of mixed types.
+
+# tuple of tuples
+
+
+"""
+Lists
+    These a grouping of data in an mutable structure. Use a list when you data is being collected or for any other
+    reason that its length might change.
+    (I think of lists as a train that can have cars added or subtracted at different stations along it journey)
+    Lists are enclosed in square brackets [] and the values are separated by commas, the values in a list are
+    accessed using integers starting with 0. Access that last value in the list starting with -1.
+"""
+
+# list example
+
+# the empty list
+
+# The append method
+
+# the extend method
+
+# list of tuples
+
+# get the last value
+
+"""
+Dictionaries
+    A dictionary is a way to store data using key-value pairs. While a list or a tuple use an integer to access
+    stored data, a dictionary value is assessed using a key, which can be an integer, string, float, or
+    even more complex data types.
+    Dictionaries are enclosed in curly brackets {} and have and use a colon : to link
+    key:value pairs. The key value pairs are separated by commas. To get a list of all the keys
+    in a given dictionary, use the .keys() method.
+"""
+
+# dictionary example
+
+# empty dictionary
+
+# add a key value pair
+
+# get all the keys for a dictionary
+
+
+"""
+The for loop
+"""
+
+# using the range function
+
+# using a list
+
+# using enumerate
+
+# with a break
+
+# with an else
+
+
+"""
+the try statement 
+"""
+
+
+
+
+"""
+A definition
+"""
+
+
+
+
