26c0c2a37b5b07016a46cfbc9eb8acec62f97a95
Caleb Wheeler <chw3k5@gmail.com>
2017-07-05 10:37:50 -0500

    Adding an introduction to python script.

--- intoToPython.py
+++ intoToPython.py
@@ -1,17 +1,21 @@
 """
-Comments
+Comments - This is a multiline comment
 """
 
+# this is a single line comment
 
 """
 The print state statement
 """
 
 # simple example
+print "Here is a simple print example."
 
 # compound example
+print "here is number", 99, 'here is another string'
 
 # the \n (newline) character
+print '\nThis newline character \n', "here is a 2nd line\n"
 
 
 
@@ -20,24 +24,38 @@ Numbers - basic single value numbers Integers and floats.
 """
 
 ### integers (no decimal point)
+anInt = 9
+notherInt = 7
 
-# Tricky division
+# ** is the power operator
+print anInt ** notherInt
+
+# Tricky divstion
+print anInt / notherInt
 
 # The modulus operator
-
-
+counter = 17
+loopLen = 6
+print "this modulus", counter % 6, "this how many times some is divided", counter / loopLen
 
 
 ### float (floating point number, has a decimal point)
-
+float1 = 55.8
+float2 = 63.0
+float3 = 1.4563e-122
 # intuitive division.
+print '\nintuitive division', float1 / float2
+print type(float2), type(anInt)
 
 # conversion to an integer
+print type(int(float1)), int(float1), int(round(float1))
 
 # conversion from an integer
 
-# when used with an integer in math
+print type(float(anInt)), float(anInt)
 
+# when used with an integer in math
+print float1 ** float2, float3, "\n"
 
 
 
@@ -51,13 +69,21 @@ Strings
 """
 
 # single quotes
+stringVar = 'This is Caleb'
+print stringVar
 
 # double quotes
+stringVar = "This is Caleb 2"
+print stringVar
+stringVar = "This is Caleb's string, ain't it grand?"
+print stringVar
 
 # conversion to a number
+aNumberString = '99'
+print int(aNumberString), float(aNumberString), type(aNumberString)
 
 # concatenation
-
+print stringVar + ' A number I like -> ',  aNumberString, "\n"
 # split method
 
 # replace method
@@ -68,18 +94,28 @@ Tuples - pronounced 2-ple
  (I think of it as a container is not allowed to changed)
  Tuples are enclosed in round brackets () and the values are separated by commas, the values in a tuple are
  accessed using integers starting with 0. Access that last value in the tuple starting with -1.
-
 """
 
 # coordinates example
+threeSpace = (99, 55, 8.7)
+print threeSpace
 
 # unpacking and packing
+x, y, z = threeSpace
+print x, y, z
 
 # calling a single value.
+print threeSpace[0], 'is the first value'
 
 # tuple of mixed types.
+bankInfo = ('Caleb', '0938583892', -88.8)
+print bankInfo, 'is my bank info'
+name, bankAccountNum, balance = bankInfo
+print "$" + str('%2.2f' % balance) + " is my balance"
 
 # tuple of tuples
+tupOfTup = (threeSpace, bankInfo)
+print tupOfTup, '\n'
 
 
 """
@@ -92,12 +128,22 @@ Lists
 """
 
 # list example
+basicList = ['rrrrrr', 5, 'rrrrrr']
+print basicList
 
 # the empty list
+basicList = []
+print basicList
 
 # The append method
+basicList = ['rrrrrr', 5, 'rrrrrr']
+basicList.append('aString')
+print basicList
 
 # the extend method
+diffList = [63498, "skejkg", 77777, ('he', 4)]
+basicList.extend(diffList)
+print basicList, '\n'
 
 # list of tuples
 
@@ -114,12 +160,21 @@ Dictionaries
 """
 
 # dictionary example
+aDict = {'apple':'is a piece of red fruit'}
+print aDict['apple']
 
 # empty dictionary
+aDict = {}
 
 # add a key value pair
+aDict['4'] = ' this is a string of stuff'
+aDict['puppy'] = ' Here is my pup!'
+aDict[7.7] = 'This key is a float'
+print aDict
+print aDict[7.7]
 
 # get all the keys for a dictionary
+print aDict.keys()
 
 
 """
@@ -138,7 +193,7 @@ The for loop
 
 
 """
-the try statement 
+the try statement
 """
 
 
